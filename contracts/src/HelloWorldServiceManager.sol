32baf9315e6e5845641d41896f2e7c9addd03aeb
William Wang <bwilliamwang@gmail.com>
2025-02-15 13:43:56 -0800

    working

--- contracts/src/HelloWorldServiceManager.sol
+++ contracts/src/HelloWorldServiceManager.sol
@@ -1,12 +1,10 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-import {ECDSAServiceManagerBase} from
-    "@eigenlayer-middleware/src/unaudited/ECDSAServiceManagerBase.sol";
+import {ECDSAServiceManagerBase} from "@eigenlayer-middleware/src/unaudited/ECDSAServiceManagerBase.sol";
 import {ECDSAStakeRegistry} from "@eigenlayer-middleware/src/unaudited/ECDSAStakeRegistry.sol";
 import {IServiceManager} from "@eigenlayer-middleware/src/interfaces/IServiceManager.sol";
-import {ECDSAUpgradeable} from
-    "@openzeppelin-upgrades/contracts/utils/cryptography/ECDSAUpgradeable.sol";
+import {ECDSAUpgradeable} from "@openzeppelin-upgrades/contracts/utils/cryptography/ECDSAUpgradeable.sol";
 import {IERC1271Upgradeable} from "@openzeppelin-upgrades/contracts/interfaces/IERC1271Upgradeable.sol";
 import {IHelloWorldServiceManager} from "./IHelloWorldServiceManager.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
@@ -17,7 +15,10 @@ import {TransparentUpgradeableProxy} from "@openzeppelin/contracts/proxy/transpa
  * @title Primary entrypoint for procuring services from HelloWorld.
  * @author Eigen Labs, Inc.
  */
-contract HelloWorldServiceManager is ECDSAServiceManagerBase, IHelloWorldServiceManager {
+contract HelloWorldServiceManager is
+    ECDSAServiceManagerBase,
+    IHelloWorldServiceManager
+{
     using ECDSAUpgradeable for bytes32;
 
     uint32 public latestTaskNum;
@@ -48,7 +49,6 @@ contract HelloWorldServiceManager is ECDSAServiceManagerBase, IHelloWorldService
         address _stakeRegistry,
         address _rewardsCoordinator,
         address _delegationManager
-
     )
         ECDSAServiceManagerBase(
             _avsDirectory,
@@ -69,8 +69,13 @@ contract HelloWorldServiceManager is ECDSAServiceManagerBase, IHelloWorldService
     // NOTE: this function creates new task, assigns it a taskId
     function createNewTask(
         string memory name
-        // TODO: add insurance claim details
-    ) external returns (Task memory) {
+    )
+        external
+        returns (
+            // TODO: add insurance claim details
+            Task memory
+        )
+    {
         // TODO: create a new task struct
         Task memory newTask;
         newTask.name = name;
@@ -91,7 +96,7 @@ contract HelloWorldServiceManager is ECDSAServiceManagerBase, IHelloWorldService
         bytes memory signature,
         bool isApproved
     ) external {
-        uint8 approvalThreshold = 2;
+        uint8 approvalThreshold = 3;
         // check that the task is valid, hasn't been responsed yet, and is being responded in time
         require(
             keccak256(abi.encode(task)) == allTaskHashes[referenceTaskIndex],
@@ -102,26 +107,32 @@ contract HelloWorldServiceManager is ECDSAServiceManagerBase, IHelloWorldService
             allTaskResponses[msg.sender][referenceTaskIndex].length == 0,
             "Operator has already responded to the task"
         );
+        require(
+            taskResponseCount[referenceTaskIndex] <= approvalThreshold,
+            "Approval Threshold has already been met."
+        );
 
-        
-        // Increment response counter
-        taskResponseCount[referenceTaskIndex]++;
-        
-        // If approved, increment approval counter
-        if (isApproved) {
-            taskApprovalCount[referenceTaskIndex]++;
+        unchecked {
+            // Increment response counter
+            taskResponseCount[referenceTaskIndex]++;
+
+            // If approved, increment approval counter
+            if (isApproved) {
+                taskApprovalCount[referenceTaskIndex]++;
+            }
         }
-
         // If we have x responses, emit the approval rate
         if (taskResponseCount[referenceTaskIndex] == approvalThreshold) {
-            uint8 approvalRate = uint8((taskApprovalCount[referenceTaskIndex] * 100) / approvalThreshold);
-            
+            uint8 approvalRate = uint8(
+                (taskApprovalCount[referenceTaskIndex] * 100) /
+                    approvalThreshold
+            );
+
             // Store response
             allTaskResponses[msg.sender][referenceTaskIndex] = signature;
-            
+
             // Emit approval rate of task response
             emit TaskResponded(approvalRate, task, msg.sender);
         }
     }
-
 }
